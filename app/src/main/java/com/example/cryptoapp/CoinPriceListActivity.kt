f6ff41e5594ac1e6db05120379ec41c47ab240e3
art <adpm@list.ru>
2022-08-09 19:46:51 +0600

    Greate main UI

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -2,9 +2,13 @@ package com.example.cryptoapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.adapters.CoinInfoAdapter
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
 
@@ -13,17 +17,19 @@ class CoinPriceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener{
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
 
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-
         viewModel.priceList.observe(this, Observer {
-            Log.d("TEST_OF_LOADING_DATA", "Success on activity: $it")
-
+           adapter.coinInfoList = it
         })
-        viewModel.getDetailInfo("BTC").observe(this, Observer {
-            Log.d("TEST_OF_LOADING_DATA", "Success on activity: $it")
 
-        })
 
     }
 
